d1f04612c2522ec8da3776be3525e40fdd147776
koneyacine <koneyacine127@gmail.com>
2023-08-11 13:35:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error 
+ * main - A c program that prints a line to the standard error
  * Return: Always 1.
  */
 int main(void)
