88c86b23f0465c8bbada3718081a0b893743dede
koneyacine <koneyacine127@gmail.com>
2023-08-15 17:54:16 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,37 +1,20 @@
 #include "main.h"
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer
- * The main function is the entry point of the program.
- *
- * It prints a greeting message and returns 0 to indicate successful execution.
- *
- * @return 0 indicating successful execution.
- * Return: ...
+ * jack_bauer- prints every minute of the day
  */
 void jack_bauer(void)
-int main(void)
 {
-int a, b, c, d;
-for (a = 0; a <= 2; a++)
+int min, hr;
+for (hr = 0; hr < 23; hr++)
 {
-for (b = 0; b <= 9; b++)
+for (min = 0; min < 59; min++)
 {
-if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-{
-for (c = 0; c <= 5; c++)
-{
-for (d = 0; d <= 9; d++)
-{
-_putchar(a + '0');
-_putchar(b + '0')
-_putchar(58);
-_putchar(c + '0');
-_putchar(d + '0');
+_putchar((hr / 10) + 48);
+_putchar((hr % 10) + 48);
+_putchar(':');
+_putchar((min / 10) + 48);
+_putchar((min % 10) + 48);
 _putchar('\n');
 }
 }
-}
-}
-}
-return (0);
 }
