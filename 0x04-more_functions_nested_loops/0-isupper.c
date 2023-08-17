47a875a2458edf3358cc7c7f00a3cd2c502695b8
koneyacine <koneyacine127@gmail.com>
2023-08-17 13:55:42 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+# include "main.h"
+
+/* 
+ * _issupper  - check if letter is supper	
+ * @x : the number to be cheked
+ * return :1 for upper letter or 0  for any else
+ *  */
+
+init _issupper(int x)
+
+{
+	if (x >=65 && x <=90 )
+	{
+		return (10;
+				}
+	return(0);	
+	}	
