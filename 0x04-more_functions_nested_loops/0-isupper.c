a77b1cfc42b87a6b0ac47e2fc4083fa9d4e8423f
koneyacine <koneyacine127@gmail.com>
2023-08-17 16:33:43 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,16 @@
-# include "main.h"
+#include "main.h"
 
-/* 
- * _issupper  - check if letter is supper	
- * @x : the number to be cheked
- * return :1 for upper letter or 0  for any else
- *  */
+/** 
+ * _issupper  - check if letter is uppercase       
+ * @x: the character to be checked
+ * Return: 1 for uppercase letter or 0 for any else
+ */
 
-init _issupper(int x)
-
+int _issupper(int x)
 {
-	if (x >=65 && x <=90 )
-	{
-		return (10;
-				}
-	return(0);	
-	}	
+    if (x >= 65 && x <= 90)
+    {
+        return (1);
+    }
+      return(0);
+}
