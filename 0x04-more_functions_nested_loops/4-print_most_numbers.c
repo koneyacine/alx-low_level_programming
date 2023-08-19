412deb2a3b7dced69018909b07f1beb4ac263faa
koneyacine <koneyacine127@gmail.com>
2023-08-18 18:39:45 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+  #include "main.h"
+
+/**
+ * print_numbers - Print the numbers since 0 up to 9
+ * return: The numbers since 0 up to 9
+ */
+
+void print_numbers(void)
+
+{
+
+        int i;
+
+        for (i = 0; i <= 9; i++)
+        {
+		if (i != 2 && i	!= 4)
+		{
+       			 _putchar(i + '0');
+       		 }
+		
+        _putchar('\n');
+}
