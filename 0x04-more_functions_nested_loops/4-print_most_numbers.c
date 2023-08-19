986f57aee7b6d6f8afa248501a95e55fb34a8c9d
koneyacine <koneyacine127@gmail.com>
2023-08-18 19:20:21 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,10 +13,15 @@ void print_numbers(void)
 
         for (i = 0; i <= 9; i++)
         {
-		if (i != 2 && i	!= 4)
+		if (i == 2 || i	== 4)
 		{
-       			 _putchar(i + '0');
-       		 }
+		continue;
+
+       		}
+		else
+		{
+		_putchar(i + '0');
+		}
 	}
 
         _putchar('\n');
