724faf6fe59c2d37d8327be21b6b25e496d15678
koneyacine <koneyacine127@gmail.com>
2023-08-18 19:37:05 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,17 @@ void more_numbers(void)
 {
     int i, j;
 
-    for (i = 0; i < 10; i++) // Repeat 10 times
+    for (i = 0; i < 10; i++)
     {
-        for (j = 0; j <= 14; j++) // Print numbers from 0 to 14
-        {
-            if (j >= 10) // For numbers greater than or equal to 10, print both digits
-            {
-                _putchar('0' + j / 10);
-            }
-            _putchar('0' + j % 10);
-        }
-        _putchar('\n');
+    for (j = 0; j <= 14; j++) 
+    {
+    if (j >9)
+    {
+    _putchar('0' + j / 10);
+    }
+    _putchar('0' + j % 10);
+    }
+    _putchar('\n');
     }
 }
 
