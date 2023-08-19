74fb2f4702f94f41c40423eeb823419998cb380b
koneyacine <koneyacine127@gmail.com>
2023-08-18 18:42:39 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-  #include "main.h"
+#include "main.h"
 
 /**
  * print_numbers - Print the numbers since 0 up to 9
@@ -17,6 +17,7 @@ void print_numbers(void)
 		{
        			 _putchar(i + '0');
        		 }
-		
+	}
+
         _putchar('\n');
 }
