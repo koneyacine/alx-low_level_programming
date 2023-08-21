544ad274dbc7d0832e2c92cfac30062d4363f64f
koneyacine <koneyacine127@gmail.com>
2023-08-21 16:54:45 -0700

    a

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ int _putchar(char c);
 void reset_to_98(int *n);	
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void puts(char *str);
+void custom_puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
