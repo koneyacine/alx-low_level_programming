707a13966f198983a97982dfe57bfd6d9908ec13
koneyacine <koneyacine127@gmail.com>
2023-08-21 19:52:15 -0700

    a

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,14 +4,13 @@
 #define PASSWORD_LENGTH 63
 int main(void)
 {
- srand(time(NULL)); 
+srand(time(NULL));
 char valid_characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-char password[PASSWORD_LENGTH + 1];
-int i;
+char password[PASSWORD_LENGTH + 1];int i;
 for (i = 0; i < PASSWORD_LENGTH; i++)
 {
 int index = rand() % (sizeof(valid_characters) - 1);
-indexpassword[i] = valid_characters[index];
+password[i] = valid_characters[index];
 }
 password[PASSWORD_LENGTH] = '\0'; // Null-terminate the password
 printf("%s\n", password);
