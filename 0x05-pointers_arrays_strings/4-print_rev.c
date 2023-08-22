f4a03ca6f1c9e23365997ad5a56e894cec33464c
koneyacine <koneyacine127@gmail.com>
2023-08-21 17:46:09 -0700

    a

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_rev - Prints a string in reverse.
+* @s: Pointer to the string.
+*/
+void print_rev(char *s)
+{
+int length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+for (int i = length - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
