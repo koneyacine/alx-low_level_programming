57daa42ad861d80970151d4c2161cd02b12f6f87
koneyacine <koneyacine127@gmail.com>
2023-08-22 05:20:53 -0700

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,15 +7,28 @@
 int _atoi(char *s)
 {
 int sign = 1;
-int result = 0;int i = 0;
-while (s[i] != '\0'){
-if (s[i] == '-')sign *= -1;
-if (s[i] >= '0' && s[i] <= '9'){
-result = result * 10 + (s[i] - '0'); 
-if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+int result = 0;
+int i = 0;
+while (s[i] != '\0') {
+if (s[i] == '-') {
+sign *= -1;
+}
+if (s[i] >= '0' && s[i] <= '9') {
+int digit = s[i] - '0';
+if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)) {
+if (sign == 1) {
+return INT_MAX;
+} 
+else 
+{
+return INT_MIN;
+}
+}
+result = result * 10 + digit;
+if (!(s[i + 1] >= '0' && s[i + 1] <= '9')) {
 break;
 }
-i++;
+}i++;
 }
 return result * sign;
 }
