e8ea0194a9822e3948e88ad0e52739633890d79e
koneyacine <koneyacine127@gmail.com>
2023-08-23 12:00:26 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,9 +2,7 @@
 char *_strcat(char *dest, char *src)
 {
 int dest_length = 0 ;
-int i ;
 int j= 0 ;
-char *c[]= "" ;
 while( dest[dest_length] != '\0')
 {
 dest_length++ ;
@@ -12,6 +10,7 @@ dest_length++ ;
 while (str[j] != 0)
 {
 dest[dest_length]= str[j] ;
+dest_length++ ;
 j ++ ;
 }
 dest[j+dest_length1] = '\0' ;
