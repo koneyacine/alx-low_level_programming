1531711cfa07cd6bd29b937305946e4206b8c5f6
koneyacine <koneyacine127@gmail.com>
2023-08-23 19:20:42 -0700

    a

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,10 @@
 nclude "main.h"
 /**
- * reverse_array - Reverses the content of an array of integers.
- * @a: An array of integers.
- * @n: The number of elements in the array.
- *
- * Return: Nothing (void).
- */
+* reverse_array - Reverses the content of an array of integers.
+* @a: An array of integers.
+* @n: The number of elements in the array.
+* Return: Nothing (void).
+*/
 void reverse_array(int *a, int n)
 {
 int tem ;
