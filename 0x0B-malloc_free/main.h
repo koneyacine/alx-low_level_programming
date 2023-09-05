24c3073eb41468a0eb128c60ee28e35b5a3b113b
koneyacine <koneyacine127@gmail.com>
2023-09-05 05:52:14 -0700

    a

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_h
+
+#include <stdlib.h>
+#include <stdio.h>
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
@@ -5,3 +11,5 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+
+#endif /*MAIN_H */
