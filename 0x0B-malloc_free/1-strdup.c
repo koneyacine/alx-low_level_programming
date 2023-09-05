0bd1dc74be407ea69a18a48687cad6debbbe8a41
koneyacine <koneyacine127@gmail.com>
2023-09-05 07:13:14 -0700

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+*This function creates a dynamically allocated copy of a string
+*/
+
+char *_strdup(char *str)
+int size;	
+char *copie;
+{
+if (str == NULL)
+{
+return NULL;
+}
+size = strlen(str);
+*copie = (char *)malloc(size);
+if (copie = NULL)
+{
+strcpy(copie , str);
+}
+return (copie);
+}
