0aa3007d2ae830ab6ab1fc23a947846e8cd39ddb
koneyacine <koneyacine127@gmail.com>
2023-09-05 05:39:20 -0700

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,12 +9,12 @@
 */
 char *create_array(unsigned int size, char c)
 char *array;
-array = (char *)malloc(size * sizeof(char));
 {
 if (size == 0)
 {
 return (NULL);
 }
+array = (char *)malloc(size * sizeof(char));
 if (array == NULL)
 {
 return (NULL);
