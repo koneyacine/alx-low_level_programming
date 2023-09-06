09107533ab809de3e03962110a74da8f82bfdafd
koneyacine <koneyacine127@gmail.com>
2023-09-06 11:38:53 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,14 +1,34 @@
 #include "main.h"
 /**
-* Concatenates two strings of characters and returns a pointer to the resulting string.
-* @param s1 The first string of characters.
-* @param s2 The second string of characters to concatenate to the first one.
-* @return A pointer to the resulting string (dynamically allocated) or NULL in case of failure.
+* str_concat - Concatenates two strings.
+*
+* This function concatenates two input strings and returns a dynamically
+* allocated new string containing the result.
+*
+* @s1: The first input string.
+* @s2: The second input string.
+*
+* Return: A pointer to the resulting string or NULL if memory allocation fails.
 */
 char *str_concat(char *s1, char *s2)
 {
-size_t len1 = strlen(s1);
-size_t len2 = strlen(s2);
+size_t len2;
+size_t len1;
+if (s1 == NULL && s2 == NULL)
+{
+return (NULL);
+}
+if (s1 == NULL){
+len1 = 0;
+else
+len1 = strlen(s1);
+if (s2 == NULL)
+{
+len2 = 0;
+else
+{
+len2 = strlen(s2);
+}
 char *result = (char *)malloc(len1 + len2 + 1);
 if (result == NULL)
 {
