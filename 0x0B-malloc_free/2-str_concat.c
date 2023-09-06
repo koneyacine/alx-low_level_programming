e232153858adb52b4b1c5b7a283727c1f9efaef4
koneyacine <koneyacine127@gmail.com>
2023-09-06 11:05:32 -0700

    a

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* Concatenates two strings of characters and returns a pointer to the resulting string.
+* @param s1 The first string of characters.
+* @param s2 The second string of characters to concatenate to the first one.
+* @return A pointer to the resulting string (dynamically allocated) or NULL in case of failure.
+*/
+char *str_concat(const char *s1, const char *s2)
+{
+size_t len1 = strlen(s1);
+size_t len2 = strlen(s2);
+char *result = (char *)malloc(len1 + len2 + 1);
+if (result == NULL)
+{
+return (NULL);
+}
+strcpy(result, s1);
+strcat(result, s2);
+return (result);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
