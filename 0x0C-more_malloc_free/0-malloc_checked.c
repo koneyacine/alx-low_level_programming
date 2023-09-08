1af0891b5626a6d123069f34f9a789908dd01dc5
koneyacine <koneyacine127@gmail.com>
2023-09-08 06:43:17 -0700

    a

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,16 @@
 #include "main.h"
-/*
-* @b :malloc_cheked function that allocate memory using
-* malloc and exit if failed
-*@b :int
-* return : pointeur to the allocated memory or NULL
+
+/**
+* function_name - One-line description
+* @parameter1: Description of parameter 1
+* @parameter2: Description of parameter 2
+*
+* Longer description (if needed) that provides more details
+* about the function and its behavior.
+*
+* Return: Description of the return value (if any)
 */
+
 
 void *malloc_checked(unsigned int b)
 {
