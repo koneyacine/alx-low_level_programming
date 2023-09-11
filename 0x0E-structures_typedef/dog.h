0031433b32104710630473e1c5c3a598780804ef
koneyacine <koneyacine127@gmail.com>
2023-09-11 13:06:15 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+* struct dog - Represents information about a dog.
+* @name: The name of the dog.
+* @age: The age of the dog.
+* @owner: The owner's name of the dog.
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
