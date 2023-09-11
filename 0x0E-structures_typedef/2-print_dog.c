419967233bcb845f2c777fc38beb2b7177f42c75
koneyacine <koneyacine127@gmail.com>
2023-09-11 15:28:55 -0700

    a

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,10 +1,12 @@
 #include "dog.h"
 #include <stdio.h>
+
 /**
 * print_dog - Prints information about a dog structure
 * @d: Pointer to the dog structure.
 * Return: void
 */
+
 void print_dog(struct dog *d)
 {
     if (d)
