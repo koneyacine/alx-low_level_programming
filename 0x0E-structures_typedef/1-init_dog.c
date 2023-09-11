bb997b628f59df1f6d6f83adc8f8ad32c783e5bb
koneyacine <koneyacine127@gmail.com>
2023-09-11 13:44:47 -0700

    a

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,15 +7,9 @@
 * @owner: Owner of the dog.
 * Description: This function initializes a dog structure with provided values.
 */
-
-
-
-
-
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-(d *).name = name;
-(d *).age = age;
-(d *).owner = owner;
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
