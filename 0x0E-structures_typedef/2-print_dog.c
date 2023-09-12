052b0917f20c613e1aacbb66e90febc97aa72cb6
koneyacine <koneyacine127@gmail.com>
2023-09-11 17:12:28 -0700

    d

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,13 +9,12 @@
  * If an element of d is NULL, it prints (nil) instead.
  * If d is NULL, it prints nothing.
  */
-
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", d->name !=NULL ? d->name : "nil");
+		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
 		printf("Age: %.6f\n", d->age);
-		printf("Owner: %s\n", d->owner != NULL ? d->owner : "nil");
+		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 	}
 }
