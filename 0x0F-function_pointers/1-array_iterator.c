64a0a317154f9060a4b059309c3ac02866b0698b
koneyacine <koneyacine127@gmail.com>
2023-09-13 12:13:02 -0700

    a

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 int *en = array + size -1;
 if (array && size && action)
 {
-while(array <= *en)
+while(array <= en)
 {
 action(*array++);
 }
