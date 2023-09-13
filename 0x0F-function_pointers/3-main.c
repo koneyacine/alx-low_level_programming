e08fd9428c0b89a819a721db8ad164a76436f080
koneyacine <koneyacine127@gmail.com>
2023-09-13 16:13:04 -0700

    a

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+/**
+ * main - This is the main function of the program.
+ *
+ * It takes command-line arguments, performs a calculation based on the
+ * provided operation, and prints the result.
+ *
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ * Return: 0 on success, an error code on failure.
+ */
+
 int main(int argc, char *argv[])
 {
 int num1, num2, result;
