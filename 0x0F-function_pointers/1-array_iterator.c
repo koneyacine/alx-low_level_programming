466bdccfb5741946dbdf2cc393e497ff79b9842a
koneyacine <koneyacine127@gmail.com>
2023-09-13 09:54:42 -0700

    a

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+*array_iterator - function that call action on evry element of 
+*array
+*@ array: pointere to the array
+*@size: length of the array
+* Return: Nothing
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int *en = array + size -1;
+if (array && size && action)
+{
+while(array <= en)
+{
+action(array++)
+}
+}
+}
