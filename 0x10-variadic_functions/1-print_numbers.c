97b8a0ff44e71d5a09f27d2c755cd89e0ea61094
koneyacine <koneyacine127@gmail.com>
2023-09-14 16:52:44 -0700

    a

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,8 @@
 #include "variadic_functions.h"
 
 /**
-* print_numbers - prints numbers, separated by a separator, followed by a new line
+* print_numbers - prints numbers, separated by
+* a separator, followed by a new line
 * @separator: the string to be printed between numbers
 * @n: the number of integers passed to the function
 * @...: variable number of integers to print
