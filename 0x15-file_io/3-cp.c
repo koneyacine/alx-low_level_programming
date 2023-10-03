76d17501f3ae6687ec8dac7d2661848b787268b9
koneyacine <koneyacine127@gmail.com>
2023-10-03 04:57:40 -0700

    restoring

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,60 +7,53 @@
 #define ERR_NOCLOSE_FORMAT "Error: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
+/**
+ * main - program
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 1 on success 0 on failure
+ */
+
 int main(int ac, char **av)
 {
 int from_fd, to_fd;
 ssize_t bytes_read, bytes_written;
 char buffer[READ_BUF_SIZE];
-
 if (ac != 3)
-{
-dprintf(STDERR_FILENO, USAGE_FORMAT);
-return (EXIT_FAILURE);
-}
+	dprintf(STDERR_FILENO, USAGE_FORMAT);
+	return (EXIT_FAILURE);
 
 from_fd = open(av[1], O_RDONLY);
 if (from_fd == -1)
-{
-dprintf(STDERR_FILENO, ERR_NOREAD_FORMAT, av[1]);
-return (EXIT_FAILURE);
-}
+	dprintf(STDERR_FILENO, ERR_NOREAD_FORMAT, av[1]);
+	return (EXIT_FAILURE);
 
 to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 if (to_fd == -1)
-{
-dprintf(STDERR_FILENO, ERR_NOWRITE_FORMAT, av[2]);
-close(from_fd);
-return (EXIT_FAILURE);
-}
-
+	dprintf(STDERR_FILENO, ERR_NOWRITE_FORMAT, av[2]);
+	close(from_fd);
+	return (EXIT_FAILURE);
 
 while ((bytes_read = read(from_fd, buffer, READ_BUF_SIZE)) > 0)
-{
-bytes_written = write(to_fd, buffer, bytes_read);
-if (bytes_written != bytes_read)
-{
-dprintf(STDERR_FILENO, ERR_NOWRITE_FORMAT, av[2]);
-close(from_fd);
-close(to_fd);
-return (EXIT_FAILURE);
-}
-}
+	bytes_written = write(to_fd, buffer, bytes_read);
+	if (bytes_written != bytes_read)
+		dprintf(STDERR_FILENO, ERR_NOWRITE_FORMAT, av[2]);
+		close(from_fd);
+		close(to_fd);
+		return (EXIT_FAILURE);
 
-if (bytes_read == -1)
-{
+	if (bytes_read == -1)
 
-dprintf(STDERR_FILENO, ERR_NOREAD_FORMAT, av[1]);
-close(from_fd);
-close(to_fd);
-return (EXIT_FAILURE);
-}
+		dprintf(STDERR_FILENO, ERR_NOREAD_FORMAT, av[1]);
+		close(from_fd);
+		close(to_fd);
+		return (EXIT_FAILURE);
 
 if (close(from_fd) == -1 || close(to_fd) == -1)
 {
 dprintf(STDERR_FILENO, ERR_NOCLOSE_FORMAT, errno);
 return (EXIT_FAILURE);
 }
-
 return (EXIT_SUCCESS);
 }
